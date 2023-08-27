e4304b2b773f20decc7fde802783cb7098e820f8
gasanchik <gasanchik09@gmail.com>
2023-08-27 17:40:20 +0200

    Implementing MyMapLoader

--- shared/src/com/hasanchik/shared/map/MyMapLoader.java
+++ shared/src/com/hasanchik/shared/map/MyMapLoader.java
@@ -8,24 +8,27 @@ import com.badlogic.gdx.assets.loaders.FileHandleResolver;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Array;
 
-public class MyMapLoader extends AsynchronousAssetLoader {
+public class MyMapLoader extends AsynchronousAssetLoader<MyMap, MyMapLoader.MyMapParameters> {
+    static public class MyMapParameters extends AssetLoaderParameters<MyMap> {
+    }
+
     //TODO: implement this class
     public MyMapLoader(FileHandleResolver resolver) {
         super(resolver);
     }
 
     @Override
-    public void loadAsync(AssetManager manager, String fileName, FileHandle file, AssetLoaderParameters parameter) {
-
-    }
-
-    @Override
-    public Object loadSync(AssetManager manager, String fileName, FileHandle file, AssetLoaderParameters parameter) {
+    public Array<AssetDescriptor> getDependencies(String fileName, FileHandle file, MyMapParameters parameter) {
         return null;
     }
 
     @Override
-    public Array<AssetDescriptor> getDependencies(String fileName, FileHandle file, AssetLoaderParameters parameter) {
+    public void loadAsync(AssetManager manager, String fileName, FileHandle file, MyMapParameters parameter) {
+
+    }
+
+    @Override
+    public MyMap loadSync(AssetManager manager, String fileName, FileHandle file, MyMapParameters parameter) {
         return null;
     }
 }
